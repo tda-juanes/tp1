1db9f22c0933d956b57f421df4f882e6d320b174
Manu Diaz <mdiaz@edrans.com>
2023-09-14 01:03:51 -0300

    added main to read from stdin and output to stdout

--- sol.py
+++ sol.py
@@ -1,9 +1,9 @@
 from collections import namedtuple
-import datetime as dt
 import heapq
-
-# type(Demora.*) = dt.timedelta
+import sys
+# type(Demora.*) = int
 Demora = namedtuple("Demora", ['s', 'a'])
+Demora.__repr__ = lambda self: f'{self.s},{self.a}'
 
 # overload comparison operators
 Demora.__eq__ = lambda self, other: self.a == other.a
@@ -16,13 +16,28 @@ Demora.__gt__ = lambda self, other: self.a < other.a
 
 def ordenar(datos: list[Demora]):
     heapq.heapify(datos)
-    return [heapq.heappop(datos) for i in range(len(datos))]
+    return [heapq.heappop(datos) for _ in range(len(datos))]
 
 
 def max_demora(videos):
-    total = m = dt.timedelta(0)
+    total = m = 0
     for s, a in videos:
         total += s
         m = max(m, total + a)
 
     return m
+
+
+def main():
+    try:
+        sys.stdin.readline() # skip headers
+        videos = [Demora(*map(int, line.split(','))) for line in sys.stdin]
+        print('\n'.join( map(str, ordenar(videos)) ))
+    except TypeError:
+        print('Invalid input: Expected 2 comma separated arguments per line', file=sys.stderr)
+    except ValueError:
+        print('Invalid input: Arguments must be valid integers', file=sys.stderr)
+
+
+if __name__ == '__main__':
+    main()
